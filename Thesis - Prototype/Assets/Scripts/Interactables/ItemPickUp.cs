d4313dc75b8abbfe1531cbdfe8cc63ad6691f4ab
DESKTOP-659UAKS\AR <ardcerezo@gmail.com>
2018-09-29 13:25:50 +0800

    asd

--- Thesis - Prototype/Assets/Scripts/Interactables/ItemPickUp.cs
+++ Thesis - Prototype/Assets/Scripts/Interactables/ItemPickUp.cs
@@ -2,10 +2,19 @@
 
 public class ItemPickUp : Interactable {
 
+	public ItemPickUp instance;
 	public Item item;	// Item to put in the inventory if picked up
 
 	// When the player interacts with the item
 	
+	void Awake ()
+	{
+		if (instance != null)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
 	void Start () 
 	{
 		
---
+++ Thesis - Prototype/Assets/Scripts/SingletonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonManager : MonoBehaviour {
+
+	public static SingletonManager instance;
+
+	void Awake ()
+	{
+		if (instance != null)
+		{
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+	}
+}
